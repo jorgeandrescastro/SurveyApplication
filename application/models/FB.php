6b302c30dfb6807193fb0d00a0bd8d964141c76d
Jorge Castro <jacastrom@gmail.com>
2015-01-06 15:18:45 -0500

    testcode

--- application/models/FB.php
+++ application/models/FB.php
@@ -108,9 +108,7 @@ class Application_Model_FB
         $hometown = $me->getProperty('hometown');
         print_r($hometown);
         print_r('/');
-        print_r($hometown['name']);
-        print_r('/');
-        print_r($hometown->getProperty('name'));
+        print_r($hometown->getName());
         print_r("-----------");
         print_r($me->getProperty('work'));
         die();
