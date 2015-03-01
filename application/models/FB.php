51c184b1fbc54cc15146373b3b7f1318463d4e98
Jorge Castro <jacastrom@gmail.com>
2015-03-01 16:19:55 -0500

    debug

--- application/models/FB.php
+++ application/models/FB.php
@@ -55,7 +55,7 @@ class Application_Model_FB
      */
     protected $_permissions = array('public_profile', 'user_about_me', 
                                     'user_education_history', 'user_hometown',
-                                    'user_work_history', 'user_birthday', 'invitable_friends');   
+                                    'user_work_history', 'user_birthday', 'user_friends');   
     
     /**
      * Constructor of the FB class
