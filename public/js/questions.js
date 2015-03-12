afc2ef4f8fe181d79c33c69f344fe6de0cbf5ab9
Jorge Castro <jacastrom@gmail.com>
2015-03-11 22:41:58 -0500

    fix on js - colors

--- public/js/questions.js
+++ public/js/questions.js
@@ -35,71 +35,77 @@ $(document).ready(function() {
     });
   }
 
-  $('input:radio[name=question_20]').click(function() {
-    var val = $('input:radio[name=question_20]:checked').val();
-    
-    if(val == 'Si') {
-      $(".dependent20").each(function (index){
-        $(this).removeClass('hidden');
-      }); 
-    }
+  if($("input:radio[name=question_20]").length ) {
+    $('input:radio[name=question_20]').click(function() {
+      var val = $('input:radio[name=question_20]:checked').val();
+      
+      if(val == 'Si') {
+        $(".dependent20").each(function (index){
+          $(this).removeClass('hidden');
+        }); 
+      }
 
-    if(val == 'No') {
-      $(".dependent20").each(function (index){
-        $(".dependent20 :input").val('');
-        $(this).addClass('hidden');
-      }); 
-    } 
-  });
+      if(val == 'No') {
+        $(".dependent20").each(function (index){
+          $(".dependent20 :input").val('');
+          $(this).addClass('hidden');
+        }); 
+      } 
+    });
+  }
 
-  $('input:radio[name=question_23]').click(function() {
-    var val = $('input:radio[name=question_23]:checked').val();
-    
-    if(val == 'Si') {
-      $(".dependent23").each(function (index){
-        $(this).removeClass('hidden');
-      }); 
-    }
+  if($("input:radio[name=question_23]").length ) {
+    $('input:radio[name=question_23]').click(function() {
+      var val = $('input:radio[name=question_23]:checked').val();
+      
+      if(val == 'Si') {
+        $(".dependent23").each(function (index){
+          $(this).removeClass('hidden');
+        }); 
+      }
 
-    if(val == 'No') {
-      $(".dependent23").each(function (index){
-        $(".dependent23 :input").val('');
-        $(this).addClass('hidden');
-      }); 
-    } 
-  });
+      if(val == 'No') {
+        $(".dependent23").each(function (index){
+          $(".dependent23 :input").val('');
+          $(this).addClass('hidden');
+        }); 
+      } 
+    });
+  }
 
-  $('input:radio[name=question_26]').click(function() {
-    var val = $('input:radio[name=question_26]:checked').val();
-    
-    if(val == 'No') {
-      $(".dependent26").each(function (index){
-        $(this).removeClass('hidden');
-      }); 
-    }
+  if($("input:radio[name=question_26]").length ) {
+    $('input:radio[name=question_26]').click(function() {
+      var val = $('input:radio[name=question_26]:checked').val();
+      
+      if(val == 'No') {
+        $(".dependent26").each(function (index){
+          $(this).removeClass('hidden');
+        }); 
+      }
 
-    if(val == 'Si') {
-      $(".dependent26").each(function (index){
-        $(".dependent26 :input").val('');
-        $(this).addClass('hidden');
-        $('.dependent26 :radio').prop('checked', false);
-        $('.dependent26 :checkbox').prop('checked', false);
-      }); 
-      $(".dependent33").each(function (index){
-        $(this).addClass('hidden');
-        $('.dependent33 :radio').prop('checked', false);
-        $(".dependent33 :input").val('');
-      });
-      $(".dependent41").each(function (index){
-        $(".dependent41 :input").val('');
-        $(this).addClass('hidden');
-      });
-      $(".dependent44").each(function (index){
-        $(".dependent44 :input").val('');
-        $(this).addClass('hidden');
-      });
-    } 
-  });
+      if(val == 'Si') {
+        $(".dependent26").each(function (index){
+          $(".dependent26 :input").val('');
+          $(this).addClass('hidden');
+          $('.dependent26 :radio').prop('checked', false);
+          $('.dependent26 :checkbox').prop('checked', false);
+        }); 
+        $(".dependent33").each(function (index){
+          $(this).addClass('hidden');
+          $('.dependent33 :radio').prop('checked', false);
+          $(".dependent33 :input").val('');
+        });
+        $(".dependent41").each(function (index){
+          $(".dependent41 :input").val('');
+          $(this).addClass('hidden');
+        });
+        $(".dependent44").each(function (index){
+          $(".dependent44 :input").val('');
+          $(this).addClass('hidden');
+        });
+      } 
+    });
+  }
 
 
   if($("#question_33").length ) {
@@ -133,39 +139,43 @@ $(document).ready(function() {
     });
   }
 
-  $('input:radio[name=question_41]').click(function() {
-    var val = $('input:radio[name=question_41]:checked').val();
-    
-    if(val == 'Si') {
-      $(".dependent41").each(function (index){
-        $(this).removeClass('hidden');
-      }); 
-    }
+  if($("input:radio[name=question_41]").length ) {
+    $('input:radio[name=question_41]').click(function() {
+      var val = $('input:radio[name=question_41]:checked').val();
+      
+      if(val == 'Si') {
+        $(".dependent41").each(function (index){
+          $(this).removeClass('hidden');
+        }); 
+      }
 
-    if(val == 'No') {
-      $(".dependent41").each(function (index){
-        $(".dependent41 :input").val('');
-        $(this).addClass('hidden');
-      }); 
-    } 
-  });
+      if(val == 'No') {
+        $(".dependent41").each(function (index){
+          $(".dependent41 :input").val('');
+          $(this).addClass('hidden');
+        }); 
+      } 
+    });
+  }
 
-  $('input:radio[name=question_44]').click(function() {
-    var val = $('input:radio[name=question_44]:checked').val();
-    
-    if(val == 'Si') {
-      $(".dependent44").each(function (index){
-        $(this).removeClass('hidden');
-      }); 
-    }
+  if($("input:radio[name=question_44]").length ) {
+    $('input:radio[name=question_44]').click(function() {
+      var val = $('input:radio[name=question_44]:checked').val();
+      
+      if(val == 'Si') {
+        $(".dependent44").each(function (index){
+          $(this).removeClass('hidden');
+        }); 
+      }
 
-    if(val == 'No') {
-      $(".dependent44").each(function (index){
-        $(".dependent44 :input").val('');
-        $(this).addClass('hidden');
-      }); 
-    } 
-  });
+      if(val == 'No') {
+        $(".dependent44").each(function (index){
+          $(".dependent44 :input").val('');
+          $(this).addClass('hidden');
+        }); 
+      } 
+    });
+  }
 
 
 
@@ -183,11 +193,5 @@ $(document).ready(function() {
     });
   }
 
-  $("#surveyBlock_1").submit(function( event ) {
-    // var $input = $(this).find("select[name=question_11]");
-    // $input.val("You can buy me love");
-    // $('#question_57').val("VAmos A ver");
-    // console.log("life is a song");
-  });
       
 });
